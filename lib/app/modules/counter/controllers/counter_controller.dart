cbbf57776daa32904ccbe7eb3eb79af792667e70
Muharafi Dalilah <dealgendut@gmail.com>
2024-02-19 14:49:23 +0700

    learn Getx and make form

---
+++ lib/app/modules/counter/controllers/counter_controller.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class CounterController extends GetxController {
+  //TODO: Implement CounterController
+
+  final count = 1.obs;
+
+  void increment() {
+    count.value++;
+    if (count > 100) {
+      Get.snackbar("Over Heat", "Dont add more or your laptop will explode",
+          colorText: Colors.black,
+          backgroundColor: Colors.redAccent[700],
+          duration: Duration(seconds: 1));
+      count.value--;
+    }
+  }
+
+  void decrement() {
+    count.value--;
+    if (count < 1) {
+      Get.snackbar("less", "you cannot do less anymore than 0",
+          colorText: Colors.black,
+          backgroundColor: Colors.redAccent[700],
+          duration: Duration(seconds: 1));
+      count.value++;
+    }
+  }
+}
---
+++ lib/app/modules/counter/views/counter_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/counter_controller.dart';
+
+class CounterView extends GetView<CounterController> {
+  CounterView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CounterView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Obx(() {
+                  return Text(
+                    "${controller.count}",
+                    style: TextStyle(fontSize: controller.count.toDouble()),
+                  );
+                })
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    controller.increment();
+                  },
+                  child: Icon(Icons.add),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      controller.decrement();
+                    },
+                    child: Icon(Icons.remove))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
